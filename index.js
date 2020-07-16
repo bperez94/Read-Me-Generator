3edfd2ef8b3c80aa6cc2404e0194c10bb126418f
Brandon Perez <brandonp940@gmail.com>
2020-07-16 10:32:36 -0400

    structure for questions added

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 // module export link to generate markdown
 const generateMarkdown = require("./utils/generateMarkdown");
+console.log(generateMarkdown);
 // inquireer and file share links
 const inquirer = require("inquirer");
 const fs = require("fs");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    },
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    },
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    },
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    },
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    },
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    },
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    },
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    },
+    {
+        type: "input",
+        Message:"",
+        name:"",
+    }
 ];
 
 // function to write README file
