d8f71a879b2b7eac8976ba8e80a4540b2aedd95a
Brandon Perez <brandonp940@gmail.com>
2020-07-16 09:43:04 -0400

    started markup for html

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <title>Read Me</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1>Read Me</h1>
+  </div>
+  </div>
+</body>
+</html>
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown};
