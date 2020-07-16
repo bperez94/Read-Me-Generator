cdd6bd2abe1e4bfeed774c5a089149b49b14e4c9
Brandon Perez <brandonp940@gmail.com>
2020-07-16 09:42:04 -0400

    linked files and added inquirer and fs

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// module export link to generate markdown
+const generateMarkdown = require("./utils/generateMarkdown");
+// inquireer and file share links
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // array of questions for user
 const questions = [
 
