3aea193d93a539d28d448e5f882d7d1c32dcc089
Brandon Perez <brandonp940@gmail.com>
2020-07-18 00:51:54 -0400

    fixed prompts, file now runs in git bash, needs fix for not readind modulized file

--- index.js
+++ index.js
@@ -1,67 +1,74 @@
-// module export link to generate markdown
-const generateMarkdown = require("./utils/generateMarkdown");
-console.log(generateMarkdown);
-// inquireer and file share links
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+const genMarkdown = require('./utils/generateMarkdown');
 
+const writeFileAsync = util.promisify(fs.writeFile);
 // array of questions for user
-const questions = [
-    {
-        type: "input",
-        Message:"",
-        name:"",
-    },
-    {
-        type: "input",
-        Message:"",
-        name:"",
-    },
-    {
-        type: "input",
-        Message:"",
-        name:"",
-    },
-    {
-        type: "input",
-        Message:"",
-        name:"",
-    },
-    {
-        type: "input",
-        Message:"",
-        name:"",
-    },
-    {
-        type: "input",
-        Message:"",
-        name:"",
-    },
-    {
-        type: "input",
-        Message:"",
-        name:"",
-    },
-    {
-        type: "input",
-        Message:"",
-        name:"",
-    },
-    {
-        type: "input",
-        Message:"",
-        name:"",
+function UserPrompt() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name:"title",
+            Message:"What is your Project TitLe?",
+        },
+        {
+            type: "input",
+            name:"date",
+            Message:"What day did you creat your application?", 
+        },
+        {
+            type: "input",
+            name:"decription",
+            Message:"Describe what your apllication does and how it works?", 
+        },
+        {
+            type: "input",
+            name:"install",
+            Message:"Are there any set up or installation requirments?",
+        },
+        {
+            type: "input",
+            name:"Bugs",
+            Message:"Are there any bugs or pronblems with the apllication?",
+        },
+        {
+            type: "input",
+            name:"contact",
+            Message:"Please share your contact info?",
+        },
+        {
+            type: "input",
+            name:"tech",
+            Message:"What Technologies were used",
+        },
+        {
+            type: "input",
+            name:"contribution",
+            Message:"Provide a link to you deployed application",
+        },
+        {
+            type: "input",
+            name:"License",
+            Message:"Provide you copy right",// also needs list with options for license
+        }
+        ]);
     }
-];
+    
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
-// function to initialize program
-function init() {
 
-}
+UserPrompt()
+    // function to write README file
+ .then(function writeToFile(genMarkdown,data) {
+    var html = genMarkdown(data);
+    return writeFileAsync("index.html",html);
+})
+.then(function() {
+    console.log("You completed your read me!")
+})
+.catch(function(err) {
+    console.log(err)
+});
 
-// function call to initialize program
-init();
+
